42e563c4b2f832d75bd5e1fcbc5df00e26bcb9d7
sumaa007 <liujiezr@foxmail>
2018-08-14 11:58:54 +0800

    后台管理=>订单-发货|bug详情页多了个/

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -18,4 +18,7 @@ Route::group([
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    // 订单列表|详细
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
