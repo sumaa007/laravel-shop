5ab39acc34c6ee8d37c68572e51ca27db908d9bb
sumaa007 <liujiezr@foxmail>
2018-08-14 13:19:42 +0800

    确认收货

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
         Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
+        // 确认收货
+        Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
     });
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
