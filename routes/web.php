dc049eddfc4e6c27c7cac92054a3d1f2670df7da
sumaa007 <liujiezr@foxmail>
2018-08-14 15:41:06 +0800

    管理后台=>同意退款=>微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,5 @@ Route::group(['middleware' => 'auth'], function() {
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
+// 微信退款回调
+Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
